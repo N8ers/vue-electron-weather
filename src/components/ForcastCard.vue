af2caf91cec66a9ec4f1c14352ca55cd0654a6dd
Nathan <nathanraysheryak@gmail.com>
2021-05-02 12:29:59 -0400

    create weather Forecast Card

---
+++ src/components/ForcastCard.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <h4>date: {{ date }}</h4>
+    <div>{{ forcast.high }}&deg;F</div>
+    <div>{{ forcast.low }}&deg;F</div>
+    <img :src="icon" alt="Weather icon">
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import { format } from 'date-fns'
+
+export default {
+  name: 'Settings',
+  components: { },
+  props: { 
+    forcast: {
+      type: Object,
+      required: true,
+      default: () => {}
+    }
+  },
+  data: function () {
+    return {  }
+  },
+  methods: {  },
+  computed: {
+    date: function () {
+      return format(Date.now(), 'EEEE, MMM do')
+    },
+    icon: function () {
+      return `http://openweathermap.org/img/w/${this.forcast.icon}.png`;
+    },
+    ...mapState({
+      // forcast: state => state.forcast
+    })
+  },
+  mounted () {  },
+  created() {  }
+}
+</script>
