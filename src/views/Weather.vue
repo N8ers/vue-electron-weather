d045af40d49c04acccd269f14d7b6ff065e8d3ea
n8ers <nathanraysheryak@gmail.com>
2021-05-07 06:33:40 -0400

    styling

--- src/views/Weather.vue
+++ src/views/Weather.vue
@@ -7,9 +7,10 @@
 
     <v-container class="grey lighten-5">
       <v-row>
-        <v-col cols="8" md="4">
+        <v-col cols="6" md="4">
           <current-weather />
         </v-col>
+        <v-spacer></v-spacer>
         <v-col cols="4" md="4">
           <v-btn color="success">Update Weather</v-btn>
         </v-col>
@@ -43,22 +44,22 @@ export default {
   methods: {
     getWeather: function () {
       // with out network: uses dummyPayload.json
-      let data = require('../../dummyPayload.json')
-      this.$store.dispatch('cleanWeatherData', data)
+      // let data = require('../../dummyPayload.json')
+      // this.$store.dispatch('cleanWeatherData', data)
 
 
       // with network
-      // this.$http.get(`forecast?q=${this.stateCity}&units=imperial&appid=${this.stateApiKey}`)
-      //   .then(response => {
-      //     this.$store.dispatch('cleanWeatherData', response.data)
-      //   })
-      //   .catch((error) => {
-      //     let payload = {
-      //       type: 'error',
-      //       message: error.message
-      //     }
-      //     this.$store.commit('setWeatherAlert', payload)
-      //   });
+      this.$http.get(`forecast?q=${this.stateCity}&units=imperial&appid=${this.stateApiKey}-`)
+        .then(response => {
+          this.$store.dispatch('cleanWeatherData', response.data)
+        })
+        .catch((error) => {
+          let payload = {
+            type: 'error',
+            message: error.message
+          }
+          this.$store.commit('setWeatherAlert', payload)
+        });
     }
   },
   computed: {
